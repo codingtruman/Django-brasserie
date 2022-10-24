023188a68635618d95ec3fb6e2a88c7d7152492a
codingtruman <maplewong04@gmail.com>
2022-10-24 17:13:39 +0200

    ready for PR

--- barproject/api/paginators.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination
-
-class CustomPagination(PageNumberPagination):
-    page_size = 10
-    def get_paginated_response(self, data):
-        return Response(data)
